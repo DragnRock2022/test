a8830aeb9ca94a9bf71a3f168ab984d27ec3f0a7
DragnRock2022 <109328830+DragnRock2022@users.noreply.github.com>
2022-07-15 13:26:25 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "4b62da4a-9467-48f2-929d-038c9197a0ca",
                         "alterId": 0
                     }
                 ],
